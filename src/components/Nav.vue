5f1347fafae8343b62a8c39433688dd7ed3aa4c8
LarryKameZhao <larryzhao1994@outlook.com>
2020-03-07 16:27:33 +0800

    create icon component

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,18 @@
 <template>
   <div class="nav">
     <router-link to="/money">
-      <svg>
-        <use xlink:href="#label" />
-      </svg>
-      记账</router-link
-    >| <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>
+      <Icon name="dollar"></Icon>记账
+    </router-link>|
+    <router-link to="/labels">
+      <Icon name="label"></Icon>标签
+    </router-link>|
+    <router-link to="/statistics">
+      <Icon name="statistic"></Icon>统计
+    </router-link>
   </div>
 </template>
 <script lang="ts">
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
-  requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context('../assets/icons', true, /\.svg$/));
-} catch (error) {
-  console.log(error);
-}
-import Vue from 'vue';
+import Vue from "vue";
 export default Vue.extend({});
 </script>
 <style lang="scss" scoped>
