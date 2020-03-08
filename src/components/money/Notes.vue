d448f77ef80f78e02b9705e71a09e9b0f2ae62a4
LarryKameZhao <larryzhao1994@outlook.com>
2020-03-08 19:44:48 +0800

    money components

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input placeholder="在这里输入备注" type="text" />
+    </label>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "notes"
+});
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: block;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">指出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "types"
+});
+</script>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  li {
+    width: 50%;
+    line-height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected {
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+}
+</style>
