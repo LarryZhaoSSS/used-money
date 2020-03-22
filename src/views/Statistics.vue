e4faefc2488036c271c56fd1d009cf173be183be
LarryKameZhao <larryzhao1994@outlook.com>
2020-03-22 11:04:19 +0800

    grouped list

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="statistics">
     <Layout>
-      <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"></Tabs>
-      <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"></Tabs>
+      <Tabs
+        class-prefix="type"
+        :data-source="typeList"
+        :value.sync="type"
+      ></Tabs>
+      <Tabs
+        class-prefix="interval"
+        :data-source="intervalList"
+        :value.sync="interval"
+      ></Tabs>
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-          {{group}}
-          <h3 class="title">{{group.title}}</h3>
+        <li v-for="(group, index) in groupedList" :key="index">
+          <h3 class="title">
+            {{ beautify(group.title) }} <span>￥{{ group.total }}</span>
+          </h3>
           <ol>
-            <li class="record" v-for="item in group.items" :key="item.id">
-              <span>{{tagString(item.tags)}}</span>
-              <span class="notes" :style="{marginRight:'auto'}">{{item.notes}}</span>
-              <span>${{item.amount}}</span>
+            <li v-for="item in group.items" :key="item.id" class="record">
+              <span>{{ tagString(item.tags) }}</span>
+              <span class="notes">{{ item.notes }}</span>
+              <span>￥{{ item.amount }} </span>
             </li>
           </ol>
         </li>
@@ -20,38 +29,84 @@
   </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-import Tabs from "@/components/Tabs.vue";
-import { Component } from "vue-property-decorator";
-import { intervalList } from "@/enums/constants/intervalList";
-import { typeList } from "@/enums/constants/recordTypeList";
+import Vue from 'vue';
+import Tabs from '@/components/Tabs.vue';
+import { Component } from 'vue-property-decorator';
+import { intervalList } from '@/enums/constants/intervalList';
+import { typeList } from '@/enums/constants/recordTypeList';
+import dayjs from 'dayjs';
 @Component({
   components: { Tabs }
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? "无" : tags.join(",");
+    const result = tags.map(item => item.name).join(',');
+    return tags.length === 0 ? '无' : result;
+  }
+  beautify(string: string) {
+    const now = dayjs();
+    const day = dayjs(string);
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('MM-DD');
+    } else {
+      return day.format('YYYY-MM-DD');
+    }
   }
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
-  get result() {
-    const { recordList } = this;
-    type HashTableValue = { title: string; items: RecordItem[] };
-    const hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createdAt!.split("T");
-      hashTable[date] = hashTable[date] || { title: date, items: [] };
-      hashTable[date].items.push(recordList[i]);
-    }
-    return hashTable;
+  clone<T>(data: T): T {
+    return JSON.parse(JSON.stringify(data));
   }
-  type = "-";
-  interval = "day";
+  get groupedList() {
+    const { recordList } = this;
+    if (recordList.length === 0) {
+      return [];
+    }
+    const newList = this.clone<RecordItem[]>(recordList)
+      .filter(r => r.type === this.type)
+      .sort(
+        (a: any, b: any) =>
+          dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
+      );
+    type Result = { title: string; total?: number; items: RecordItem[] }[];
+    const result: Result = [
+      {
+        title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'),
+        items: [newList[0]]
+      }
+    ];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({
+          title: dayjs(current.createdAt).format('YYYY-MM-DD'),
+          items: [current]
+        });
+      }
+    }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        console.log(sum);
+        console.log(item);
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
+  }
+  type = '-';
+  interval = 'day';
   intervalList = intervalList;
   typeList = typeList;
   beforeCreate() {
-    this.$store.commit("fetchRecords");
+    this.$store.commit('fetchRecords');
   }
 }
 </script>
