460d9819c62c9d676bf336cbf68c2ab530b7ff25
LarryKameZhao <larryzhao1994@outlook.com>
2020-03-22 19:26:42 +0800

    fix cannot get tags for recordList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,34 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <number-pad @update:value="onUpdateAmount" @submit="saveRecord"></number-pad>
+      <number-pad
+        @update:value="onUpdateAmount"
+        @submit="saveRecord"
+      ></number-pad>
       <Tabs :data-source="recordTypeList" :value.sync="record.type"></Tabs>
       <div class="notes">
-        <form-item field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"></form-item>
+        <form-item
+          field-name="备注"
+          placeholder="在这里输入备注"
+          @update:value="onUpdateNotes"
+        ></form-item>
       </div>
 
-      <tags :data-source="tags"></tags>
+      <tags :data-source="tags" :value.sync="record.tags"></tags>
     </Layout>
   </div>
 </template>
 <script lang="ts">
-import Vue from "vue";
-import NumberPad from "@/components/money/number-pad.vue";
-import FormItem from "@/components/money/FormItem.vue";
-import Tags from "@/components/money/Tags.vue";
-import Tabs from "@/components/Tabs.vue";
-import { Component, Watch } from "vue-property-decorator";
-import { typeList } from "@/enums/constants/recordTypeList";
+import Vue from 'vue';
+import NumberPad from '@/components/money/number-pad.vue';
+import FormItem from '@/components/money/FormItem.vue';
+import Tags from '@/components/money/Tags.vue';
+import Tabs from '@/components/Tabs.vue';
+import { Component, Watch } from 'vue-property-decorator';
+import { typeList } from '@/enums/constants/recordTypeList';
 
-window.localStorage.setItem("version", "0.0.1");
-import store from "@/store/index2";
+window.localStorage.setItem('version', '0.0.1');
+import store from '@/store/index2';
 
 @Component({
   components: { Tags, FormItem, NumberPad, Tabs },
@@ -34,10 +41,10 @@ import store from "@/store/index2";
 export default class Money extends Vue {
   tags = store.tagList;
   // recordList: RecordItem[] = store.recordList;
-  record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
+  record: RecordItem = { tags: [], notes: '', type: '-', amount: 0 };
   recordTypeList = typeList;
   created() {
-    this.$store.commit("fetchRecords");
+    this.$store.commit('fetchRecords');
   }
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -50,7 +57,7 @@ export default class Money extends Vue {
   }
   saveRecord() {
     // store.createRecord(this.record);
-    this.$store.commit("createRecord", this.record);
+    this.$store.commit('createRecord', this.record);
   }
 }
 </script>
@@ -61,7 +68,7 @@ export default class Money extends Vue {
 }
 </style>
 <style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
+@import '~@/assets/style/helper.scss';
 .notes {
   padding: 12px 0;
 }
